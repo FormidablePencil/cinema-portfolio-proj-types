4c301af62a14a3353fa13d911f6489ddaacb4d40
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-19 14:25:57 -0700

    test

---
+++ index.ts
@@ -0,0 +1,52 @@
+export interface portfolioContentT {
+  tabSectionTitles: {
+    [index: number]: {
+      tabTitle: string;
+      subSections?: { [index: number]: { tabTitle: string } };
+    };
+  };
+  introParagraph: string;
+  videoProjects: {
+    [index: number]: {
+      url: string;
+      alt: string;
+    };
+    length: any;
+    map: Function;
+  };
+  tech: {
+    [index: number]: {
+      icon: string;
+      video: string;
+      alt: string;
+    };
+    length: any;
+    map: Function;
+  };
+  moreTech: {
+    [index: number]: {
+      icon: string;
+      alt: string;
+    };
+    length: any;
+    map: Function;
+  };
+  photoshop: {
+    [index: number]: {
+      url: string;
+      alt: string;
+    };
+    length: any;
+    map: Function;
+  };
+  bio: {
+    title: string;
+    paragraph: string;
+  };
+  contacts: {
+    email: string;
+    github: string;
+    linkedin: string;
+  };
+}
+export default portfolioContentT;
